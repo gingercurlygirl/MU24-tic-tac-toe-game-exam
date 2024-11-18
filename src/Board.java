077a20e2e16640f11f9eead63cd015dff1bbee46
Ivana Lovric <ivana.lovric1314@gmail.com>
2024-11-18 16:58:04 +0100

    print a nice board to terminal

---
+++ src/Board.java
@@ -0,0 +1,10 @@
+public class Board {
+
+    public static void printBoard(char[] board) {
+        System.out.println(" " + board[0] + " | " + board[1] + " | " + board[2]);
+        System.out.println("---+---+---");
+        System.out.println(" " + board[3] + " | " + board[4] + " | " + board[5]);
+        System.out.println("---+---+---");
+        System.out.println(" " + board[6] + " | " + board[7] + " | " + board[8]);
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,7 @@
+public class Game {
+
+    public Game() {
+        char[] board = {'X',' ',' ','X',' ','O',' ',' ',' '};
+        Board.printBoard(board);
+    }
+}
